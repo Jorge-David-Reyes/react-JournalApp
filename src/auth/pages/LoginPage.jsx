6d77ff9f03d3b9a71470e6e7496ed42027e4e887
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2024-05-15 12:44:12 -0600

    244 - LoginPage - Diseño - Segunda Parte

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
+import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Google } from '@mui/icons-material';
 
 export const LoginPage = () => {
   return (
@@ -37,6 +39,28 @@ export const LoginPage = () => {
                   fullWidth
                 />
               </Grid>
+
+              <Grid container spacing={2} sx={{ mb:2, mt:1 }}>
+                <Grid item xs={12} sm={6}>
+                  <Button variant='contained' fullWidth>
+                    Login
+                  </Button>
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <Button variant='contained' fullWidth>
+                    <Google />
+                    <Typography sx={{ml:1}}>Google</Typography>
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <Grid container direction='row' justifyContent='end'>
+                <Link component={ RouterLink } color='inherit' to="/auth/register">
+                  Crear una cuenta
+                </Link>
+              </Grid>
+
             </Grid>
           </form>
           
