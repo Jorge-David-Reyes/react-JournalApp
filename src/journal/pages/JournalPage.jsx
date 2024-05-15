b9eb763b40c6e5847486c751bc59ff24eae38384
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2024-05-15 14:57:43 -0600

    250 - NothingSelectedView - No hay nada seleccionado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,17 @@
 // import { MailOutline } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
-
+import { NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minima tenetur laudantium consectetur quisquam! Natus nihil reprehenderit voluptate beatae impedit atque mollitia distinctio optio? Quisquam beatae officiis illum optio adipisci.</Typography>
+      {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minima tenetur laudantium consectetur quisquam! Natus nihil reprehenderit voluptate beatae impedit atque mollitia distinctio optio? Quisquam beatae officiis illum optio adipisci.</Typography> */}
     
       {/* NothingSelected */}
+      <NothingSelectedView/>
+
       {/* NoteView */}
 
     </JournalLayout>
