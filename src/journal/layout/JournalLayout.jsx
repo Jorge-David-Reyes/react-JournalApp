8cc24a6fb41cb15c8a503ef8fe54753322aafeb7
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2024-05-15 14:40:36 -0600

    249 - SideBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material"
-import { NavBar } from "../components/NavBar"
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components"
 
 const drawerWidth = 240;
 export const JournalLayout = ({children}) => {
@@ -9,12 +9,17 @@ export const JournalLayout = ({children}) => {
         <NavBar drawerWidth={ drawerWidth }/>
 
         {/* Sidebar drawerWidth */}
+        <SideBar drawerWidth={ drawerWidth }/>
+
+        {/* Main */}
 
         <Box
             component='main'
             sx={{flexGrow:1, p: 3}}
         >
-            {/* Toolbar */}
+            
+            <Toolbar/>
+
             { children }
         </Box>
     </Box>
