edcf29e169592ff60910a2222d9e23ab8c7b5980
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2024-05-15 10:21:17 -0600

    242 - Configuracion de MUI con Vite

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,11 +1,14 @@
 import { AppRouter } from './router/AppRouter'
+import { AppTheme } from './theme'
 
 import React from 'react'
 
 export const JournalApp = () => {
   return (
     <>
+      <AppTheme>
         <AppRouter />
+      </AppTheme>
     </>
   )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,13 @@
+import { MailOutline } from "@mui/icons-material";
+import { Typography } from "@mui/material";
 
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <>
+        <Typography variant='h1'>JournalPage</Typography>
+        <MailOutline/>
+    </>
+
   )
 }
