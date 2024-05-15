9c9d74a10cf1ac75ebaea62b126c7b81abc2bdf0
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2024-05-15 09:06:39 -0600

    239 - Configuracion de Rutas principales y secundarias

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,11 @@
+import { AppRouter } from './router/AppRouter'
+
 import React from 'react'
 
 export const JournalApp = () => {
   return (
     <>
-        <h1>Hola Mundo</h1>
+        <AppRouter />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import { JournalApp } from './JournalApp.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <JournalApp/>
+    <BrowserRouter>
+      <JournalApp/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
