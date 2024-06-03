dbe9fdc8b8df9c267eed3e31fa06471f54c3a9ee
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2024-06-03 12:33:45 -0600

    280 - Disparar accion de autenticacion

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,7 @@
+import { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
+
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
 
@@ -7,13 +9,19 @@ import { AuthLayout } from '../layout/AuthLayout';
 import { useForm } from '../../hooks';
 import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
 
+
 export const LoginPage = () => {
+
+  const { status } = useSelector( state => state.auth );
+
   const dispatch = useDispatch();
 
   const {email, password, onInputChange} = useForm({
     email: 'jorge@gmail.com',
     password: '123456',
   });
+
+  const isAuthenticating = useMemo(() => status === 'checking', [status]);
 
   const onSubmit = ( event ) => {
     event.preventDefault();
@@ -56,13 +64,18 @@ export const LoginPage = () => {
 
               <Grid container spacing={2} sx={{ mb:2, mt:1 }}>
                 <Grid item xs={12} sm={6}>
-                  <Button type="submit" variant='contained' fullWidth>
+                  <Button 
+                    disabled={ isAuthenticating }
+                    type="submit" 
+                    variant='contained' 
+                    fullWidth>
                     Login
                   </Button>
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
                   <Button 
+                    disabled={ isAuthenticating }
                     variant='contained' 
                     fullWidth
                     onClick={ onGoogleSignIn }>
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,5 +1,5 @@
 import { signInWithGoogle } from '../../firebase/providers';
-import { checkingCredentials } from './';
+import { checkingCredentials, login, logout } from './';
 
 export const checkingAuthentication = ( email, password ) => {
     return async( dispatch ) => {
@@ -12,6 +12,9 @@ export const startGoogleSignIn = () => {
     return async( dispatch ) => {
         dispatch( checkingCredentials() );
         const result = await signInWithGoogle();
-        console.log({result})
+        // console.log({result})
+        if(!result.ok) return dispatch( logout( result.errorMessage) );
+        // delete result.ok;
+        dispatch(login( result ))
     }
 }
