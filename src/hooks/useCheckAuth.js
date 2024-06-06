ca55cbba2aa3e885bd4072539df262b01c8b8314
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2024-06-06 14:16:54 -0600

    292 - Custom Hook para autenticacion

---
+++ src/hooks/useCheckAuth.js
@@ -0,0 +1,21 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { logout, login } from '../store/auth';
+import { FirebaseAuth } from '../firebase/config';
+
+export const useCheckAuth = () => {
+    const {status} = useSelector(state => state.auth)
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+        onAuthStateChanged(FirebaseAuth, async(user) =>{
+            if(!user) return dispatch( logout() );
+            
+            const { uid, email, displayName, photoURL } = user;
+            dispatch( login({ uid, email, displayName, photoURL }));
+        });
+    }, []);
+
+    return status;
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,25 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { onAuthStateChanged } from 'firebase/auth'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
+
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
 import { CheckingAuth } from '../ui'
-import { FirebaseAuth } from '../firebase/config'
-import { login, logout } from '../store/auth'
+import { useCheckAuth } from '../hooks'
 
 export const AppRouter = () => {
 
-  const {status} = useSelector(state => state.auth)
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async(user) =>{
-      if(!user) return dispatch( logout() );
-      const { uid, email, displayName, photoURL } = user;
-      dispatch( login({ uid, email, displayName, photoURL }));
-    });
-  }, []);
+  const status = useCheckAuth();
 
   if(status === 'checking'){
     return <CheckingAuth/>
@@ -30,7 +18,7 @@ export const AppRouter = () => {
         {
           (status === 'authenticated')
           ? <Route path="/*" element={ <JournalRoutes/> }/>
-          : <Route path="/*" element={ <JournalRoutes/> }/>
+          : <Route path="/auth/*" element={ <AuthRoutes/> }/>
         }
         <Route path="/*" element={ <Navigate to = '/auth/login'/> }/>
 
