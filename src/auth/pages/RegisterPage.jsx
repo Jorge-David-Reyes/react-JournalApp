91882a654e9b6a51d90271e20403c51e38f65bba
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2024-06-06 15:04:38 -0600

    294 - Animaciones para la aplicacion

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -46,7 +46,7 @@ export const RegisterPage = () => {
   return (
     <AuthLayout title='Crear cuenta'>
         {/* <h1>FormValid: {isFormValid ? 'Valido': 'Incorrecto'}</h1> */}
-        <form onSubmit={ onSubmit }>
+        <form onSubmit={ onSubmit } className='animate__animated animate__fadeIn animate__faster'>
           <Grid container>
             <Grid item xs={12} sx={{mt: 2}}>
               <TextField
