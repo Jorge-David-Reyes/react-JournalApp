368a481910f0706158f4d4c491e3b26eb5f0a932
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2024-06-07 13:32:29 -0600

    300 - Preparar la base de datos - CloudFirestore

---
+++ src/store/journal/thunks.js
@@ -0,0 +1,15 @@
+export const startNewNote = () => {
+    return async( dispatch ) => {
+        // uid
+
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        }
+
+        //!dispatch
+        // dispatch( newNote )
+        // dispatch( activarNote )
+    }
+}
