129beae63000e59b59f88617c3c7fa5929ab62d6
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2024-06-14 16:26:11 -0600

    error corregido

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -17,6 +17,7 @@ export const startNewNote = () => {
         const newNote = {
             title: '',
             body: '',
+            imageUrls: [],
             date: new Date().getTime(),
         }
 
